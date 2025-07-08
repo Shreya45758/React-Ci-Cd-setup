387700637c3d56b4a39bb4f80539a1d085784f93
ShreyaWavhalENATA <shreya.wavhal@autopeepal.com>
2025-07-08 16:03:52 +0530

    lint added

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -3,19 +3,19 @@ import App from '../App';
 
 test('renders Vite + React text', () => {
     render(<App />);
-    // const headerElement = screen.getByText(/Vite \+ React/i);
-    // expect(headerElement).toBeInTheDocument();
+    const headerElement = screen.getByText(/Vite \+ React/i);
+    expect(headerElement).toBeInTheDocument();
 });
 
-// test('increments count on button click', () => {
-//     render(<App />);
-//     const buttonElement = screen.getByAltText(/count is 0/i);
-//     expect(buttonElement).toBeInTheDocument();
+test('increments count on button click', () => {
+    render(<App />);
+    const buttonElement = screen.getByAltText(/count is 0/i);
+    expect(buttonElement).toBeInTheDocument();
 
-//     fireEvent.click(buttonElement);
-//     expert(buttonElement).toHaveTextContent('count is 1');
+    fireEvent.click(buttonElement);
+    expert(buttonElement).toHaveTextContent('count is 1');
 
-//     fireEvent.click(buttonElement);
-//     expert(buttonElement).toHaveTextContent('count is 2');
+    fireEvent.click(buttonElement);
+    expert(buttonElement).toHaveTextContent('count is 2');
 
-// })
+})
